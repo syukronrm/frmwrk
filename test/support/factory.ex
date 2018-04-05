bcd09c333b5dbec10d4c3f38066155577a7e08b3
Syukron Rifail M <syukronrifai@gmail.com>
2018-04-05 09:42:03 +0700

    add ex_machine factory

---
+++ test/support/factory.ex
@@ -0,0 +1,23 @@
+defmodule Frmwrk.Factory do
+  use ExMachina.Ecto, repo: Frmwrk.Repo
+
+  alias Ecto.Date
+
+  def user_factory do
+    %Frmwrk.User{
+      first_name: "John",
+      last_name: "Doe",
+      email: sequence(:email, &"email-#{&1}@example.com"),
+    }
+  end
+
+  def campaign_factory do
+    %Frmwrk.Campaign{
+      title: "Pondok IT",
+      image_url: "http://via.placeholder.com/350x250",
+      description: "Mewujudkan pondok IT yang memajukan umat",
+      deadline: Date.cast!("2018-05-20"),
+      user: build(:user),
+    }
+  end
+end
