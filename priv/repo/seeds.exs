789f8147dcfb8050c4131e45709910afc202c567
Syukron Rifail M <syukronrifai@gmail.com>
2018-04-09 13:39:18 +0700

    initial seeder

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,58 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Ecto.Date
+
+alias Frmwrk.Repo
+
+alias Frmwrk.User
+alias Frmwrk.Campaign
+
+# SUPER_ADMIN = 1
+# ADMIN = 2
+# CAMPAIGNER = 3
+# DONATUR = 4
+
+Repo.delete_all(User)
+Repo.delete_all(Campaign)
+
+{:ok, _} = Repo.insert %User{
+  first_name: "Campaigner",
+  last_name: "User",
+  email: "usercampaigner@gmail.com",
+  role: 3
+}
+
+{:ok, _} = Repo.insert %User{
+  first_name: "John",
+  last_name: "Doe",
+  email: "johndoe@gmail.com",
+  role: 4
+}
+
+{:ok, _} = Repo.insert %Campaign{
+  title: "Campaign One",
+  short_description: "Dolore velit dolor exercitationem ut cupiditate",
+  description: "Dolore velit dolor exercitationem ut cupiditate. Quisquam eum animi numquam modi. Explicabo occaecati qui qui ea officiis. Eos eius sint enim. Laudantium dicta exercitationem ea ex. Odit est at dignissimos. Odit quas consequatur eos quos. Et eum dolorem nemo quis quo.",
+  image_url: "asdasdsa",
+  url: "campaignone",
+  deadline: Date.cast!("2018-07-20")
+}
+
+{:ok, _} = Repo.insert %Campaign{
+  title: "Campaign Two",
+  short_description: "Dolore velit dolor exercitationem ut cupiditate",
+  description: "Dolore velit dolor exercitationem ut cupiditate. Quisquam eum animi numquam modi. Explicabo occaecati qui qui ea officiis. Eos eius sint enim. Laudantium dicta exercitationem ea ex. Odit est at dignissimos. Odit quas consequatur eos quos. Et eum dolorem nemo quis quo.",
+  image_url: "asibdnjas",
+  url: "campaigntwo",
+  deadline: Date.cast!("2018-06-10")
+}
+
+{:ok, _} = Repo.insert %Campaign{
+  title: "Campaign Three",
+  short_description: "Dolore velit dolor exercitationem ut cupiditate",
+  description: "Dolore velit dolor exercitationem ut cupiditate. Quisquam eum animi numquam modi. Explicabo occaecati qui qui ea officiis. Eos eius sint enim. Laudantium dicta exercitationem ea ex. Odit est at dignissimos. Odit quas consequatur eos quos. Et eum dolorem nemo quis quo.",
+  image_url: "askdbas",
+  url: "campaignthree",
+  deadline: Date.cast!("2018-05-10")
+}
