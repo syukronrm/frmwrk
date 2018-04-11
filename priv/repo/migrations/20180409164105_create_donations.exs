eb882f46a0f9f6ac0c43ebf2d18e68c358fa0013
Syukron Rifa'il M <syukronrifai@gmail.com>
2018-04-11 15:45:00 +0700

    migration: add references

--- priv/repo/migrations/20180409164105_create_donations.exs
+++ priv/repo/migrations/20180409164105_create_donations.exs
@@ -6,6 +6,7 @@ defmodule Frmwrk.Repo.Migrations.CreateDonations do
       add :amount, :integer
       add :verified_at, :date
       add :donatur_id, references(:users, on_delete: :nothing)
+      add :campaign_id, references(:campaigns, on_delete: :nothing)
 
       timestamps()
     end
