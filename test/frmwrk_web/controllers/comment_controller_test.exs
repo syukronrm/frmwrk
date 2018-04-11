2203223b934ffd9185f52d00254ce0f1f205ed62
Syukron Rifa'il M <syukronrifai@gmail.com>
2018-04-11 19:13:40 +0700

    tests for comment and donation

--- test/frmwrk_web/controllers/comment_controller_test.exs
+++ test/frmwrk_web/controllers/comment_controller_test.exs
@@ -4,6 +4,9 @@ defmodule FrmwrkWeb.CommentControllerText do
   import Frmwrk.Factory
   alias Frmwrk.Campaigns
 
+  @valid_attrs %{text: "tes komentar"}
+  @invalid_attrs %{text: ""}
+
   def fixture(:comment) do
     user = insert(:user)
     campaign = insert(:campaign)
@@ -13,9 +16,17 @@ defmodule FrmwrkWeb.CommentControllerText do
     {:ok, campaign: campaign, comment1: comment1, comment2: comment2}
   end
 
+  def fixture(:campaign) do
+    user = insert(:user)
+    campaign = insert(:campaign)
+
+    {:ok, user: user, campaign: campaign}
+  end
+
   describe "show comments" do
     setup [:create_comment]
 
+    @tag comment: true
     test "show comments", %{conn: conn, campaign: campaign} do
       conn = get conn, campaign_path(conn, :show, campaign.url)
 
@@ -24,7 +35,58 @@ defmodule FrmwrkWeb.CommentControllerText do
     end
   end
 
+  describe "form validation" do
+    setup [:create_campaign]
+
+    @tag comment: true
+    test "show create new comment form when user is campaign's backer or campaigner", %{conn: conn, campaign: campaign, user: user} do
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :show, campaign.url), @valid_attrs)
+
+      assert html_response(conn, 200) =~ "<button type=\"submit\""
+    end
+
+    @tag comment: true
+    test "do not show new comment form when user is not logged in", %{conn: conn, campaign: campaign} do
+      conn = get conn, campaign_path(conn, :show, campaign.url)
+
+      assert html_response(conn, 200) =~ "Anda harus login terlebih dahulu"
+    end
+  end
+
+  describe "new" do
+    setup [:create_campaign]
+
+    @tag comment: true
+    test "show info flash when comment is valid", %{conn: conn, user: user, campaign: campaign} do
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :campaign_new), @valid_attrs)
+
+      assert redirected_to(conn) == campaign_path(conn, :show, campaign.url)
+      assert get_flash(conn, :info) =~ "Terima kasih atas komentar Anda"
+    end
+
+    @tag comment: true
+    test "show error flash when comment is invalid", %{conn: conn, user: user, campaign: campaign} do
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :campaign_new), @invalid_attrs)
+
+      assert redirected_to(conn) == campaign_path(conn, :show, campaign.url)
+      assert get_flash(conn, :error) =~ "Harap isi kolom komentar"
+    end
+  end
+
   defp create_comment(_) do
     comment = fixture(:comment)
+  end
+
+  defp create_campaign(_) do
+    campaign = fixture(:campaign)
   end
 end
---
+++ test/frmwrk_web/controllers/donation_controller_test.exs
@@ -0,0 +1,82 @@
+defmodule FrmwrkWeb.DonationControllerTest do
+  use FrmwrkWeb.ConnCase
+
+  import Frmwrk.Factory
+
+  @valid_attrs %{amount: 10000}
+  @invalid_attrs_1 %{amount: 100}
+  @invalid_attrs_2 %{amount: ""}
+
+  def fixture(:campaign) do
+    user = insert(:user)
+    campaign = insert(:campaign, %{title: "Menjadi Supermen", user: user})
+
+    {:ok, campaign: campaign}
+  end
+
+  describe "show" do
+    setup [:create_campaign]
+
+    @tag donation: true
+    test "show donate button", %{conn: conn, campaign: campaign} do
+      conn = get conn, campaign_path(conn, :new, campaign.url)
+
+      assert html_response(conn, 200) =~ "Donasi"
+    end
+
+    @tag donation: true
+    test "show input form", %{conn: conn, campaign: campaign} do
+      conn = get conn, campaign_path(conn, :donation_new, campaign.url)
+
+      assert html_response(conn, 200) =~ "name=\"donation[amount]\""
+    end
+  end
+
+  describe "new" do
+    setup [:create_campaign]
+
+    @tag donation: true
+    test "post donation to campaign", %{conn: conn, campaign: campaign} do
+      user = insert(:user)
+
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :campaign_confirm, campaign.url), @valid_attrs)
+
+      assert html_response(conn, 200) =~ ~r/konfirmasi/i
+      assert html_response(conn, 200) =~ ~r/transfer/i
+      assert html_response(conn, 200) =~ ~r/donasi sekarang/i
+    end
+
+    @tag donation: true
+    test "redirected to input when value is invalid", %{conn: conn, campaign: campaign} do
+      user = insert(:user)
+
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :campaign_confirm, campaign.url), @invalid_attrs)
+
+      assert get_flash(conn, :info) =~ "Masukkan nominal lebih dari Rp. 10.0000"
+      assert redirected_to(conn, 302) == campaign_path(conn, :campaign_new, campaign.url)
+    end
+
+    @tag donation: true
+    test "redirected to campaign page when donation is success", %{conn: conn, campaign: campaign} do
+      user = insert(:user)
+
+      conn =
+        conn
+        |> assign(:user, user)
+        |> post(campaign_path(conn, :campaign_create, campaign.url), @valid_attrs)
+
+      assert get_flash(conn, :info) =~ "Terima kasih atas donasi Anda"
+      assert redirected_to(conn, 302) == campaign_path(conn, :campaign_new, campaign.url)
+    end
+  end
+
+  defp create_campaign(_) do
+    campaign = fixture(:campaign)
+  end
+end
